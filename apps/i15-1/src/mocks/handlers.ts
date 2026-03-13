5d671bab20c93cab7ae987eea962e5a41370b262
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2026-03-13 14:23:54 +0000

    Use the username and see if it helps

--- apps/i15-1/src/mocks/handlers.ts
+++ apps/i15-1/src/mocks/handlers.ts
@@ -1,5 +1,5 @@
 import { http, HttpResponse, graphql } from "msw";
-import type { Person } from "../context/userAuth/authUtils";
+// import type { Person } from "../context/userAuth/authUtils";
 
 const fakeTaskId = "7304e8e0-81c6-4978-9a9d-9046ab79ce3c";
 
@@ -20,21 +20,22 @@ export const handlers = [
     return HttpResponse.json("IDLE");
   }),
 
-  http.put("/oauth2/sign_in", () => {}),
+  // http.put("/oauth2/sign_in", () => {}),
 
-  http.put("/oauth2/sign_out", () => {}),
+  // http.put("/oauth2/sign_out", () => {}),
 
   http.get("/oauth2/userinfo", (request) => {
-    const auth = request.request.headers.get("authorization");
+    // const auth = request.request.headers.get("authorization");
 
-    if (auth && auth.startsWith("Bearer ")) {
-      const user: Person = {
-        identifier: auth.slice(7),
-        accepted_orca_eula: true,
-      };
-      return HttpResponse.json(user);
-    }
+    // if (auth && auth.startsWith("Bearer ")) {
+    //   const user: Person = {
+    //     identifier: auth.slice(7),
+    //     accepted_orca_eula: true,
+    //   };
+    //   return HttpResponse.json(user);
+    // }
 
-    return new HttpResponse(null, { status: 401 });
+    // return new HttpResponse(null, { status: 401 });
+    return new HttpResponse({ preferredUsername: "nonloso" });
   }),
 ];
