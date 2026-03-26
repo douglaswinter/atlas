fd86bc22e9135bf51fe19912eba2c9c0de0cdd6f
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2026-03-26 18:07:30 +0000

    At least add loadConfiguration for a start

---
+++ packages/pvws-config/src/loadConfiguration.ts
@@ -0,0 +1,28 @@
+import { type CsWebLibConfig } from "@diamondlightsource/cs-web-lib";
+import { useEffect, useState } from "react";
+
+const configFile = "/pvwsconfig.json";
+const defaultConfig = {
+  storeMode: "PROD",
+  PVWS_SOCKET: "pvws.diamond.ac.uk",
+  PVWS_SSL: true,
+  THROTTLE_PERIOD: 100,
+};
+
+export const loadPvwsConfig = async (): Promise<CsWebLibConfig> => {
+  let config;
+  if (config) {
+    return config;
+  }
+  try {
+    // Point towards your file location
+    const response = await fetch(configFile);
+    config = await response.json();
+  } catch (error) {
+    console.warn("Configuration not found falling back to defaults", error);
+    // Set defaults here if necessary
+    config = defaultConfig;
+  }
+
+  return config as CsWebLibConfig;
+};
