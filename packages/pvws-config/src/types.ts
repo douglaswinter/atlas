a38ba6cb4021254a70e776843b396a60ae68593c
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2026-03-27 15:29:17 +0000

    Just a comment

--- packages/pvws-config/src/types.ts
+++ packages/pvws-config/src/types.ts
@@ -2,3 +2,4 @@ import { DType } from "@diamondlightsource/cs-web-lib";
 
 type NotConnected = "not connected";
 export type RawValue = DType | undefined | NotConnected;
+// NOTE - ACHTUNG! DTYPE not exported anymore in newer versions, will need to look at code to figure it out!
