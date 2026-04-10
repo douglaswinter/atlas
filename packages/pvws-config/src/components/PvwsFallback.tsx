bde657bc57ec1c8fcd2182f5808cb19921370b9c
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2026-04-10 12:55:53 +0000

    Update fallback

--- packages/pvws-config/src/components/PvwsFallback.tsx
+++ packages/pvws-config/src/components/PvwsFallback.tsx
@@ -2,14 +2,14 @@ import { TextField } from "@mui/material";
 import type { ReactNode } from "react";
 
 export function PvwsFallback(): ReactNode {
+  const errorMsg = <b>Error Connecting!</b>;
   return (
     <TextField
       color="error"
       slotProps={{
         input: { readOnly: true },
       }}
-    >
-      <b>Error Connecting!</b>
-    </TextField>
+      defaultValue={errorMsg}
+    />
   );
 }
