870ed7567515d0df9dd9d4d8a8d210ed4575e113
Relm-Arrowny <raymond.fan@diamond.ac.uk>
2026-05-20 13:18:08 +0000

    vite config.ts switch to blueapi

--- apps/p99/vite.config.ts
+++ apps/p99/vite.config.ts
@@ -1,9 +1,22 @@
+// apps/p99/vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import relay from "vite-plugin-relay";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), relay],
   define: {
     global: {},
   },
+  // redirect API calls to the backend during development
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       target: "https://p99-blueapi.diamond.ac.uk",
+  //       changeOrigin: true,
+  //       secure: false,
+  //       rewrite: path => path.replace(/^\/api/, ""),
+  //     },
+  //   },
+  // },
 });
