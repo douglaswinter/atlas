434907c41f2aeb83da96da9007b87c2fc6b93e76
Relm-Arrowny <raymond.fan@diamond.ac.uk>
2026-05-28 09:56:41 +0000

    added mock service worker

--- apps/p99/src/main.tsx
+++ apps/p99/src/main.tsx
@@ -10,6 +10,12 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BlueapiProvider } from "@atlas/blueapi-query";
 import { createApi } from "@atlas/blueapi";
 
+async function enableMocking() {
+  if (import.meta.env.DEV) {
+    const { worker } = await import("./mocks/browser");
+    return worker.start();
+  }
+}
 const router = createBrowserRouter([
   {
     path: "/",
@@ -25,15 +31,16 @@ const router = createBrowserRouter([
 
 const queryClient = new QueryClient();
 export const api = createApi("/api");
-
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <ThemeProvider theme={DiamondTheme} defaultMode="system">
-      <QueryClientProvider client={queryClient}>
-        <BlueapiProvider api={api}>
-          <RouterProvider router={router} />
-        </BlueapiProvider>
-      </QueryClientProvider>
-    </ThemeProvider>
-  </StrictMode>,
-);
+enableMocking().then(() => {
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <ThemeProvider theme={DiamondTheme} defaultMode="system">
+        <QueryClientProvider client={queryClient}>
+          <BlueapiProvider api={api}>
+            <RouterProvider router={router} />
+          </BlueapiProvider>
+        </QueryClientProvider>
+      </ThemeProvider>
+    </StrictMode>,
+  );
+});
---
+++ apps/p99/src/mocks/handlers.ts
@@ -0,0 +1,33 @@
+import { http, HttpResponse } from "msw";
+import plansResponse from "./plans-response.json";
+import devicesResponse from "./devices-response.json";
+
+const fakeTaskId = "46709394";
+export const handlers = [
+  http.get("/api/plans", () => {
+    return HttpResponse.json(plansResponse);
+  }),
+  http.get("/api/devices", () => {
+    return HttpResponse.json(devicesResponse);
+  }),
+  http.get("/api/worker/state", () => {
+    return HttpResponse.json("IDLE");
+  }),
+  http.put("/api/worker/task", () => {
+    return HttpResponse.json({
+      task_id: fakeTaskId,
+    });
+  }),
+  http.put("/api/worker/task", () => {
+    return HttpResponse.json({
+      task_id: fakeTaskId,
+    });
+  }),
+
+  http.post("/api/tasks", () => {
+    return HttpResponse.json({
+      task_id: fakeTaskId,
+      status: 201,
+    });
+  }),
+];
