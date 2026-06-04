89ec4002a8051ebc72faca0bebde7b2985aaa771
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2026-06-04 12:53:54 +0000

    Small change

--- apps/i15-1/src/queue/QueueStatusPanel.tsx
+++ apps/i15-1/src/queue/QueueStatusPanel.tsx
@@ -64,7 +64,7 @@ export function QueueStatusPanel() {
           {!connected
             ? "Not Connected"
             : empty
-              ? "Empty"
+              ? "Finished"
               : paused
                 ? "Paused"
                 : "Running"}
