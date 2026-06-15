e1e98af1ea6c2629f141246fc184ea8f4f0a66a3
Dominic Oram <dominic.oram@diamond.ac.uk>
2026-06-15 10:50:09 +0100

    Restore accidentally modified file

--- apps/i15-1/src/graphql/getSessionPlaylistQuery.generated.ts
+++ apps/i15-1/src/graphql/getSessionPlaylistQuery.generated.ts
@@ -1,34 +1,11 @@
 /** Internal type. DO NOT USE DIRECTLY. */
 type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
 /** Internal type. DO NOT USE DIRECTLY. */
-export type Incremental<T> =
-  | T
-  | {
-      [P in keyof T]?: P extends " $fragmentName" | "__typename" ? T[P] : never;
-    };
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
 export type GetSessionPlaylistQueryVariables = Exact<{
   proposal: number;
   session: number;
 }>;
 
-export type GetSessionPlaylistQuery = {
-  instrumentSession: {
-    __typename: "InstrumentSession";
-    experiments: {
-      __typename: "ExperimentConnection";
-      edges: Array<{
-        __typename: "ExperimentEdge";
-        node: {
-          __typename: "Experiment";
-          name: string;
-          sample: { __typename: "Sample"; name: string; data: unknown };
-          experimentDefinition: {
-            __typename: "ExperimentDefinition";
-            name: string;
-            data: unknown;
-          };
-        };
-      }>;
-    };
-  } | null;
-};
+
+export type GetSessionPlaylistQuery = { instrumentSession: { __typename: 'InstrumentSession', experiments: { __typename: 'ExperimentConnection', edges: Array<{ __typename: 'ExperimentEdge', node: { __typename: 'Experiment', name: string, sample: { __typename: 'Sample', name: string, data: unknown }, experimentDefinition: { __typename: 'ExperimentDefinition', name: string, data: unknown } } }> } } | null };
